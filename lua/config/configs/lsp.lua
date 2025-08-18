cb44335e0fdfac8812da97e722a2307e771d44b0
capito <seth.l.tootell@gmail.com>
2025-08-18 19:43:41 +1000

    fixed java with some auto-repair
    sometimes jdlts will create broken caches that cause it to fail to start
    (usually with exit code 13). this can be fixed by removing the cache.
    auto-repair was implemented, however it does nuke the ~/.cache/jdtls
    directory and will cause startup to be slow first time after it breaks

--- lua/config/configs/lsp.lua
+++ lua/config/configs/lsp.lua
@@ -1,15 +1,34 @@
 require("lspconfig").lua_ls.setup({
-	settings = {
-		Lua = {
-			diagnostics = { globals = { "vim" } }
-		}
-	}
+    settings = {
+        Lua = {
+            diagnostics = { globals = { "vim" } }
+        }
+    }
 })
 
 local mason_lsp = require("mason-lspconfig")
 
 mason_lsp.setup({
-	ensure_installed = { "lua_ls", "ts_ls" },
-	automatic_installation = true,
-	automatic_enable = true
+    ensure_installed = { "lua_ls", "ts_ls" },
+    automatic_installation = true,
+    automatic_enable = true
+})
+
+require("lspconfig").jdtls.setup({
+    on_exit = function(code, _, client_id)
+        vim.schedule(function()
+            if code ~= 0 then
+                local cache = vim.fn.expand("~/.cache/jdtls")
+                if vim.fn.isdirectory(cache) == 1 then
+                    vim.notify("[LSP_DEBUG] non-zero jdtls exit, clearing cache")
+                    vim.fn.delete(cache, "rf")
+                end
+            end
+        end)
+    end,
+    handlers = {
+        -- By assigning an empty function, you can remove the notifications
+        -- printed to the cmd
+        ["$/progress"] = function(_, _, _) end,
+    },
 })
