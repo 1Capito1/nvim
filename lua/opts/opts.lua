6388a57f6d355502b4ef6554fcf4b5a85d42863a
capito <seth.l.tootell@gmail.com>
2025-08-18 20:04:40 +1000

    diagnostic signs replacement

--- lua/opts/opts.lua
+++ lua/opts/opts.lua
@@ -4,3 +4,9 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.relativenumber = true
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
